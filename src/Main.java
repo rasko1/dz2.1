abcb129d0730d36630460ea9483e6b72d0946e5a
rasko1 <oophn.88@yandex.ru>
2023-03-04 20:15:08 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 32232;
+        int miles = price / 20;
+        System.out.println("За " + price + "р. Вам начислят " + miles + " миль");
+    }
+}
